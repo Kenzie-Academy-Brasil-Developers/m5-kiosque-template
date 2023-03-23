5c00d7e77371b5a7eeac8f326df34edd413739c7
Joseph <josephvriesman16@gmail.com>
2023-03-23 18:06:50 -0300

    feat: tarefa 2/finalized

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from management.product_handler import get_product_by_id
 from management.product_handler import get_products_by_type
 from management.product_handler import add_product
 from menu import products
+from management.tab_handler import calculate_tab
 
 if __name__ == "__main__":
     # Seus prints de teste aqui
@@ -15,4 +16,12 @@ if __name__ == "__main__":
         "type": "fast-food"
     }
     print(add_product(products, **new_product))
+    table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
+    table_2 = [
+        {"_id": 10, "amount": 3},
+        {"_id": 20, "amount": 2},
+        {"_id": 21, "amount": 5},
+    ]
+    print(calculate_tab(table_1))
+    print(calculate_tab(table_2))   
     ...
--- management/tab_handler.py
+++ management/tab_handler.py
@@ -1,2 +1,10 @@
+from menu import products
+
 def calculate_tab(list_dictionary):
-    return
+    list = []
+    for table in list_dictionary:
+        for list_menu in products:
+            if table["_id"] == list_menu["_id"]:
+                list.append(list_menu["price"] * table["amount"])
+    sub_total = sum(list)
+    return {"subtotal": f"${round(sub_total, 2)}"}
