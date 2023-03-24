692b28e543279a940badea5c35100b5b91c755dd
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-03-24 15:49:00 -0300

    feat:finshTask

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from management.product_handler import get_product_by_id
 from management.product_handler import get_products_by_type
+from management.product_handler import add_product
+from menu import products
+from management.tab_handler import calculate_tab
 
-# from .menu import products
 
 if __name__ == "__main__":
-    print(get_product_by_id(39))
-    print(get_products_by_type("vegetab"))
+    table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
+    table_2 = [
+        {"_id": 10, "amount": 3},
+        {"_id": 20, "amount": 2},
+        {"_id": 21, "amount": 5},
+    ]
+
+    print(calculate_tab(table_1))
+    print(calculate_tab(table_2))
     ...
