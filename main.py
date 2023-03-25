7a41a821e9cf06d4ac3f0c529c4f4e175cd6c557
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-03-24 22:11:11 -0300

    feat: creat menu_prod

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from management.product_handler import get_products_by_type
 from management.product_handler import add_product
 from menu import products
 from management.tab_handler import calculate_tab
+from management.product_handler import menu_report
 
 
 if __name__ == "__main__":
-    new_product = {
+    new_product: dict = {
         "title": "Suco de React",
         "price": 5.0,
         "rating": 4,
         "description": "Suco de React com Limao",
         "type": "drink",
     }
+    
     print(add_product(products, **new_product))
+
+    print(menu_report())
+    
+    print(get_products_by_type([1, 1, 3, 5]))
     ...
--- management/product_handler.py
+++ management/product_handler.py
@@ -20,6 +20,9 @@ def filter_products(id, key: str):
 
 
 def get_product_by_id(id: int):
+    if not isinstance(id, int):
+        raise TypeError("product id must be an int")
+     
     x = filter_products(id, "_id")
 
     if x: 
@@ -29,6 +32,8 @@ def get_product_by_id(id: int):
 
 
 def get_products_by_type(type: str):
+    if not isinstance(type, str):
+        raise TypeError("product type must be a str")
 
     x = filter_products(type, "type")
 
@@ -36,6 +41,7 @@ def get_products_by_type(type: str):
         return x
     else:
         return []
+    
     ...
 
 
@@ -59,3 +65,21 @@ def add_product(menu_list: list, **payload: dict):
     menu_list.append(payload)
 
     return payload
+
+
+def menu_report():
+    product_count = len(products)
+
+    average_price: int = 0
+
+    for element in products:
+        average_price += element["price"]
+        ...
+    
+    average_price = round(average_price / product_count, 1)
+
+    list_key = [d["type"] for d in products]
+
+    count = max(list_key, key=list_key.count)
+
+    return f"Products Count: {product_count} - Average Price: ${average_price} - Most Common Type: {count}"
