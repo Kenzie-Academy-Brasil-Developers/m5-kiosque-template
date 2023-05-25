88bf1edc01f6553614700a8d4710e272ee54da0c
Renato Gouveia <rpgouveia@gmail.com>
2023-05-25 09:40:29 -0300

    feat: add function add_product

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from management.product_handler import get_product_by_id
 from management.product_handler import get_products_by_type
+from management.product_handler import add_product
+from menu import products
+
+new_product1 = {
+    "title": "Bolinho JS",
+    "price": 1.0,
+    "rating": 2,
+    "description": "Bolinho de JS com cenoura",
+    "type": "bakery",
+}
 
 if __name__ == "__main__":
     print(get_product_by_id(28))
     print(get_products_by_type("drink"))
+    print(add_product(products, new_product1))
--- management/product_handler.py
+++ management/product_handler.py
@@ -4,7 +4,7 @@ def get_product_by_id(product_id):
     for product in products:
         if product["_id"] == product_id:
             return product
-    return None
+    return {}
 
 def get_products_by_type(product_type):
     matching_products = []
@@ -12,3 +12,13 @@ def get_products_by_type(product_type):
         if product["type"] == product_type:
             matching_products.append(product)
     return matching_products
+
+def add_product(product_list, new_product):
+    if product_list:
+        max_id = max(product_list, key = lambda x: x["_id"])
+        new_id = max_id["_id"] + 1
+    else:
+        new_id = 1
+    product = {"_id": new_id, **new_product}
+    product_list.append(product)
+    return product
