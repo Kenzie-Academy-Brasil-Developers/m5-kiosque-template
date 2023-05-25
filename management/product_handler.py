d5ac881b6bd50426c7446cf0efb53ec6a4cb4ff3
Rhyan Oliveira <rhyan1510@hotmail.com>
2023-05-25 18:01:36 -0300

    feat: refatorando e adicionando menu_report

--- management/product_handler.py
+++ management/product_handler.py
@@ -2,24 +2,23 @@ from menu import products
 
 
 def get_product_by_id(id):
-<<<<<<< HEAD
+    if not isinstance(id, int):
+        raise TypeError("product id must be an int")
+
     for product in products:
         if product["_id"] == id:
-=======
-    
-    for product in products:
-        if product['_id'] == id:
->>>>>>> 39b6850f62febd3c8e71c8f8d17972b95d905bc1
             return product
 
     return {}
 
 
 def get_products_by_type(type):
+    if not isinstance(type, str):
+        raise TypeError("product type must be a str")
+
     all_products = []
 
     for product in products:
-<<<<<<< HEAD
         if product["type"] == type:
             all_products.append(product)
 
@@ -44,12 +43,30 @@ def add_product(menu: list, **args: dict):
     menu.append(args)
 
     return args
-=======
-        if product['type'] == type:
-            all_products.append(product)
-        
-    if len(all_products) == 0:
-        return []
-    else:
-        return all_products
->>>>>>> 39b6850f62febd3c8e71c8f8d17972b95d905bc1
+
+
+def menu_report():
+    all_prices = len(products)
+    sum_of_all_prices = 0
+
+    for product in products:
+        price = product["price"]
+        sum_of_all_prices += price
+
+    media = sum_of_all_prices / all_prices
+
+    media_arredondada = round(media, 2)
+
+    contagem_types = {}
+
+    for product in products:
+        typee = product["type"]
+
+        if typee in contagem_types:
+            contagem_types[typee] += 1
+        else:
+            contagem_types[typee] = 1
+
+    type_more_common = max(contagem_types, key=contagem_types.get)
+
+    return f"Products Count: {len(products)} - Average Price: ${media_arredondada} - Most Common Type: {type_more_common}"
