964f98f498a4576e708d1c99296c9a4e37e894ee
wagner840 <wagnerlevel2@gmail.com>
2023-05-28 21:12:28 -0300

    feat: tarefa2 x finalizada

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from management.product_handler import get_product_by_id
 from management.product_handler import get_products_by_type
+from management.product_handler import add_product
+from management.tab_handler import calculated_tab
+from menu import products
 
 if __name__ == "__main__":
     # Seus prints de teste aqui
     print(get_product_by_id(1))
     print(get_products_by_type('drink'))
+    new_product = {
+        "title": "X-Python",
+        "price": 5.0,
+        "rating": 5,
+        "description": "Sanduiche de Python",
+        "type": "fast-food"
+    }
+    print(add_product(products, **new_product))
+    table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
+    table_2 = [
+        {"_id": 10, "amount": 3},
+        {"_id": 20, "amount": 2},
+        {"_id": 21, "amount": 5},
+    ]
+
+    print(calculated_tab(table_1))
+    
+
+
+
+    
